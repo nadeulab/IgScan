ad2ad44780dbce3b2d16c5afb892cae22451a496
Ian Márquez López <ian.marquez.lopez@gmail.com>
2026-06-17 13:18:40 +0200

    add own package docu

---
+++ R/IgScan-package.R
@@ -0,0 +1,32 @@
+#' IgScan: A high-throughput analysis toolkit for dissecting immune repertoires in B-cell neoplasms and beyond
+#'
+#' @description
+#' IgScan is an R package for processing and analyzing high-throughput
+#' sequencing data of B-cell immunoglobulin (Ig) repertoires, both at the
+#' bulk and single-cell levels. The single-cell workflow supports multiple
+#' data formats, including 10x Genomics, BD Rhapsody, and Parse Evercode.
+#' IgScan provides detailed immunogenetic annotation, clonotype grouping,
+#' and repertoire reconstruction, with open compatibility with AIRR
+#' standards and other widely used tools in the field.
+#'
+#' @section Useful links:
+#' \itemize{
+#'   \item Package website: \url{https://nadeulab.github.io/IgScan/}
+#'   \item Report bugs at: \url{https://github.com/nadeulab/IgScan/issues}
+#' }
+#'
+#' @author
+#' **Maintainer**: Ian Márquez-López \email{iamarquez@recerca.clinic.cat}
+#'
+#' Authors:
+#' \itemize{
+#'   \item Ian Márquez-López
+#'   \item Ferran Nadeu \email{nadeu@recerca.clinic.cat}
+#' }
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
